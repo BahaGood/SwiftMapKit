fc9b40bf77fd413493897b49fca85ce75aeadfe1
Bakdaulet <pro.dot@mail.ru>
2017-04-09 12:16:51 +0600

    Mapp Application

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,19 +7,109 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    //MARK: Properties
+    @IBOutlet weak var mainMapView: MKMapView!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    
+    @IBOutlet weak var clubLocationLabel: UILabel!
+    //Properties.variables
+    let regionRadius = 1000
+    
+    let locationManager = CLLocationManager()
+    
+    let sdu_location = CLLocationCoordinate2DMake(43.20786594848359, 76.66972160339355)
+    
+    //MARK: Methods
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //Label which show distance
+        distanceLabel.layer.cornerRadius = 25
+        
+        distanceLabel.layer.backgroundColor = UIColor(red: 201/255, green: 155/255, blue: 214/255, alpha: 1).cgColor
+        
+        //Work with map
+        
+        self.mainMapView.delegate = self
+        
+        self.mainMapView.mapType = .standard
+        
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mainMapView.showsUserLocation = true
+//        
+        let annotation = Annotations(title: "Suleyman Demirel University", locationName: "Kaskelen, Abylai Khan 1", discipline: "University", coordinate: sdu_location)
+        
+        self.mainMapView.addAnnotation(annotation)
+        
+        centerMapOnLocation(location: sdu_location)
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func centerMapOnLocation(location: CLLocationCoordinate2D){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location, CLLocationDistance(regionRadius * 2), CLLocationDistance(regionRadius * 2))
+        
+        mainMapView.setRegion(coordinateRegion, animated: true)
     }
-
-
+    
+//    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+//        
+//        let location = locations.last as! CLLocation
+//        
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        
+//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//        
+//        self.mainMapView.setRegion(region, animated: true)
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mainMapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Errors: localize description" + error.localizedDescription)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation{
+            return nil;
+        }else{
+            let pinIdent = "Pin";
+            var pinView: MKPinAnnotationView;
+            if let dequeuedView = mainMapView.dequeueReusableAnnotationView(withIdentifier: pinIdent) as? MKPinAnnotationView {
+                dequeuedView.annotation = annotation;
+                pinView = dequeuedView;
+            }else{
+                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: pinIdent);
+                
+            }
+            return pinView;
+        }
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftMapKit 
+# SwiftMapKit In this application we do that. First we find place where we click 2 second and then we show an alert which we write in this name of place and add the annotation where we click and in this app may click right and left where stay view in front of segment controller
